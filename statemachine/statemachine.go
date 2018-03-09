026a7a6afdc1cef368b34164e980bdf667ef55d3
larsXYZ <larsm.angelsen@gmail.com>
2018-03-09 14:41:40 +0100

    work work work

--- statemachine/statemachine.go
+++ statemachine/statemachine.go
@@ -1,14 +1,20 @@
 package statemachine
 
 import "./../elevio"
+import "fmt"
 
 //States
 var current_floor int = -1
 var current_target int = -1
 var current_direction elevio.MotorDirection = elevio.MD_Up
 
-//Functions
+//=======Functions=======
+
+//Initializes the statemachine
 func Init(){
+
+  //Print info
+  fmt.Printf("elevator initialization started\n")
 
   //Channels
   floor_sensors_channel  := make(chan int)
@@ -19,23 +25,26 @@ func Init(){
   //Listen to floor floor_sensors
   go elevio.PollFloorSensor(floor_sensors_channel)
 
-  //Determines current floor
+  //Determine current floor
+  init_floor_finder(floor_sensors_channel)
+
+  //Print info
+  fmt.Printf("current_floor = %d\n", current_floor)
+
+}
+
+//Determines current floor at startup
+func init_floor_finder(c chan int){
+
   elevio.SetMotorDirection(current_direction)
 
   //Wait until we hit a floor
   select{
-  case floor := <-floor_sensors_channel:
+  case floor := <-c:
     current_floor = floor
   }
 
-  //Now we stop
+  //Now we set current_floor and stop
   current_direction = elevio.MD_Stop
   elevio.SetMotorDirection(current_direction)
-
-
-
-
-
-
-
 }
